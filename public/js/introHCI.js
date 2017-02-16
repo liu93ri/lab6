82b93958afc9a3aa5fd9536cb02f76b7816574f7
liu93ri <463788672@qq.com>
2017-02-16 13:24:15 -0800

    Finish all features

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -46,5 +46,17 @@ function addProject(result) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", setColors);
+
 	console.log("User clicked on color button");
+}
+
+function setColors(passedColor) {
+	var colors = passedColor['colors']['hex'];
+	
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
